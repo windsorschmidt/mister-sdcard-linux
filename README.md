2e89aff32c8210af2ad1b8b3b053b6b83c3b8557
Windsor Schmidt <windsor.schmidt@gmail.com>
2019-04-04 11:24:03 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# mister-sdcard-linux
-Linux shell script to prepare an SD card for use with MiSTer
+# About
+
+A Linux shell script to prepare an SD card for use with MiSTer. Based on https://github.com/alanswx/SD-installer_MiSTer
+
+# Dependencies
+
+- exfat-utils
+- unrar
+
+# Usage
+
+```
+create-sdcard.sh <mister-archive> <target-device>
+```
+
+Where `<mister-archive>` is a release archive from the [MiSTer repository](https://github.com/MiSTer-devel/Main_MiSTer/tree/master/releases), and `<target-device>` is the name of the SD card block device.
+
+This script will **DESTROY** the contents of the block device passed to it; it does **not** check or confirm the device name you choose. Be careful. Must be run as root.
+
+Example:
+
+```
+sudo ./create-sdcard.sh release_20190302.rar /dev/mmcblk0
+```
+
+Note: Please wait until the script completes before removing the card, as flushing the disk cache may take some time.
---
+++ create-sdcard.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+RELARCH=$1 # MiSTer release archive
+BLKDEV=$2 # target block device that ---WILL BE OVERWRITTEN!!!---
+FILESDIR="files" # expect to find archive contents here
+UBOOTIMG="files/linux/uboot.img" # U-Boot bootloader image
+EXTRADIR="extra" # optional; contents copied to SD card root (e.g. cores)
+
+msg(){ echo "$(tput setaf 2)--- $1$(tput sgr0)"; }
+
+# for testing: create a disk image and mount as loopback device
+loop_setup(){    
+    LOOPIMG="/tmp/mister.img" # final SD card image
+
+    msg "creating fake disk image for loopback device"
+    dd if=/dev/zero of=$LOOPIMG bs=1024 count=300000
+
+    msg "using disk image with loopback device"
+    losetup $BLKDEV $LOOPIMG
+    losetup
+}
+
+loop_teardown(){
+    msg "detaching loopback device (image saved at: $LOOPIMG)"
+    losetup -d $BLKDEV
+}
+
+#loop_setup
+
+msg "creating exFAT and U-Boot SPL partitions"
+# use the entire disk:
+# reserved (1M), exFAT partition, U-Boot (1M), reserved (1M)
+SDSIZE=$((`sfdisk -s $BLKDEV` / 1024))
+FATSIZE=$((SDSIZE - 3))
+UBOOTSTART=$(($SDSIZE - 2))
+echo -e "1M,${FATSIZE}M,7\n${UBOOTSTART}M,1M,a2" | sfdisk $BLKDEV
+partprobe $BLKDEV
+
+msg "formatting exFAT partition"
+mkfs.exfat ${BLKDEV}p1
+
+msg "mounting exFAT partition"
+mkdir -p $FILESDIR
+mount ${BLKDEV}p1 $FILESDIR
+
+msg "unpacking MiSTer release archive"
+unrar x -y -x*.exe $RELARCH
+
+msg "copying extra files"
+cp -Rfv $EXTRADIR/* $FILESDIR
+
+msg "copying U-Boot to bootloader partition"
+dd if=$UBOOTIMG of=${BLKDEV}p2
+
+msg "unmounting exFAT partition (may take a minute)"
+umount $FILESDIR
+rm -rf $FILESDIR
+
+#loop_teardown
+
+msg "done"
